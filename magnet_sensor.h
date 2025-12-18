1152821b56bd8c6f6d877f0aaf25e7578aaa17ec
MAXsteel913555 <2382828997@qq.com>
2025-12-18 17:16:27 +0800

    Create magnet_sensor.h

---
+++ magnet_sensor.h
@@ -0,0 +1,17 @@
+#ifndef __MAGNET_SENSOR_H
+#define __MAGNET_SENSOR_H
+
+#include "main.h"
+
+/* 引脚定义 */
+#define MAGNET_GPIO_PORT      GPIOA
+#define MAGNET_GPIO_PIN       GPIO_PIN_1
+
+
+/* 函数声明 */
+void MagnetSensor_Init(void);
+void MagnetSensor_Update(void);
+uint8_t MagnetSensor_GetState(void);
+void MagnetSensor_Process(void);
+
+#endif /* __MAGNET_SENSOR_H */
